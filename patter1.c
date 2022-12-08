522df1820efbd1038586f9634cfa3aba8ad72244
Abhijit-codder <111674298+Abhijit-codder@users.noreply.github.com>
2022-12-09 02:29:03 +0530

    Update patter1.c

--- patter1.c
+++ patter1.c
@@ -1,6 +1,34 @@
 #include<stdio.h>
 int main()
 {
-    printf("hello india");
+int i,j,rows,column,a[100][100];
+printf("enter the number of rows=");
+scanf("%d",&rows);
+printf("enter the no. of column =");
+scanf("%d",&column);
+
+for(i=0;i<rows;i++)
+{
+    for(j=0;j<column;j++)
+    {
+        printf("enter the element=a[%d][%d]",i,j);
+        scanf("%d",&a[i][j]);
+
+    }
+    
+}
+printf("printing the matrix:\n");
+{
+    for(i=0;i<rows;i++)
+    
+    
+    {
+        printf("\n");
+        for(j=0;j<column;j++)
+    {
+        printf("%d\t",a[i][j]);
+    }
+        }
+    }
     return 0;
 }
